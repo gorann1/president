e0964d9ae0819518baf31de6917ab37561fb5f19
gorann1 <gorandevop@outlook.com>
2023-03-26 22:54:33 +0200

    #pre_nextauth

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -41,9 +41,9 @@ const Main = (props: IMainProps) => (
             <li className="mr-6">
               <a
                 className="border-none text-gray-700 hover:text-gray-900"
-                href="https://github.com/ixartz/Next-js-Boilerplate"
+                href="/contact/"
               >
-                GitHub
+                Contact
               </a>
             </li>
             <li className="mr-6">
